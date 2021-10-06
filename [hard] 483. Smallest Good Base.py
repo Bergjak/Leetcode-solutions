8af60b73d269df56dfca8df7a175f70856c031ec
Bergjak <90265317+Bergjak@users.noreply.github.com>
2021-10-06 15:13:47 -0400

    Update [hard] 483. Smallest Good Base.py
    https://leetcode.com/problems/smallest-good-base/discuss/1507074/Python-binary-search-solution-with-explained-math

--- [hard] 483. Smallest Good Base.py
+++ [hard] 483. Smallest Good Base.py
@@ -7,7 +7,7 @@ class Solution:
         # Notice that f(x, k) = 1 + x(f(x, k-1)) and f(x, k - 1) == f(x, k) - x**k. So, f(x, k) = 1 + x(f(x,k) - x**k) and we want f(x, k) = n.
         # Therefore, we have that n = 1 + x(n - x**k) which implies that n * (1 - x) == 1 - x**k which implies that n = (1 - x**k)//(1 - x), as required.
 
-        # Notice that f(x, k) for fixed k is monotonically increasing with x and same for fixed and and increasing k.
+        # Notice that f(x, k) for fixed k is monotonically increasing with x and same for fixed x and and increasing k.
         # So, perhaps we could do 2D binary search? Like, for each k we propose, we find the closest x. Or, since k will never
         # be bigger than 60 (ie, 2**60 > 10**18), we could just search for the best base x for each k and then return the lowest x that works
         # So, that's O(log(n)^2), i.e., log(n) for linear search over all possible k values and log(n) for binary searching for x
