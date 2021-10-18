f7795df0ae7b0eeb5af00b9e3bec8c97bc80ec2b
Bergjak <90265317+Bergjak@users.noreply.github.com>
2021-10-18 11:22:29 -0400

    Update [hard] 2045. Second Minimum Time to Reach Destination.py

--- [hard] 2045. Second Minimum Time to Reach Destination.py
+++ [hard] 2045. Second Minimum Time to Reach Destination.py
@@ -9,12 +9,8 @@ class Solution:
         If we do not have a 1D graph, then we still have to consider the above path as a contender. However, that should be
         the only contending path for which we revisit a node. Reasoning: The path i ==> i+1 ==> i ==> i+1 takes the same amount of time
         regardless of which node i.
-
-        Therefore, I think we can do dijkstras with (time, node) (where (time, node) will also be the visited set entry), and
-        when the first path is finished, compute the additional time taken for the cycle described above, and get the second
-        completed path (the node right after the first completed path). Then sort these three options and return the second one.
-
-        If time // change is odd, then stop. Else, go. Stopping just incurs a time penalty of change, and then we still go anyway
+        
+        If time // change is odd, then stop. Else, go. Stopping just incurs a time penalty of change, and then we still go anyway.
         '''
 
         visited = set()
